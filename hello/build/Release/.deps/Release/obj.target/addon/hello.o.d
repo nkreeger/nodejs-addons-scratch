3ab84ddfd228f2161ea3499fa511acb507837a34
Nick Kreeger <nick.kreeger@gmail.com>
2018-01-25 13:49:16 -0800

    Simple hello addon + test file.

---
+++ hello/build/Release/.deps/Release/obj.target/addon/hello.o.d
@@ -0,0 +1,13 @@
+cmd_Release/obj.target/addon/hello.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node -I/usr/local/google/home/kreeger/.node-gyp/8.9.1/src -I/usr/local/google/home/kreeger/.node-gyp/8.9.1/deps/uv/include -I/usr/local/google/home/kreeger/.node-gyp/8.9.1/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/addon/hello.o.d.raw   -c -o Release/obj.target/addon/hello.o ../hello.cc
+Release/obj.target/addon/hello.o: ../hello.cc \
+ /usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node/node.h \
+ /usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node/v8.h \
+ /usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node/v8-version.h \
+ /usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node/v8config.h \
+ /usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node/node_version.h
+../hello.cc:
+/usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node/node.h:
+/usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node/v8.h:
+/usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node/v8-version.h:
+/usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node/v8config.h:
+/usr/local/google/home/kreeger/.node-gyp/8.9.1/include/node/node_version.h:

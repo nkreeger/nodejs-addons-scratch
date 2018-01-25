f1f7a057734e807b347b6f2044c1e61497391deb
Nick Kreeger <nick.kreeger@gmail.com>
2018-01-25 15:29:20 -0800

    add float32 iterator.

--- tf-test/tf_test.cc
+++ tf-test/tf_test.cc
@@ -6,32 +6,40 @@
 //  return 0;
 //}
 
-
 #include <node.h>
 #include <tensorflow/c/c_api.h>
 
 namespace add_func {
 
-using v8::Exception;
-using v8::FunctionCallbackInfo;
-using v8::Isolate;
-using v8::Local;
-using v8::Object;
-using v8::String;
-using v8::Value;
-
-void Version(const FunctionCallbackInfo<Value> & args) {
-  Isolate *isolate = args.GetIsolate();
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, TF_Version()));
+void Version(const v8::FunctionCallbackInfo<v8::Value> & args) {
+  v8::Isolate *isolate = args.GetIsolate();
+  args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, TF_Version()));
 }
 
-void Init(Local<Object> exports) {
+void ArrayTest(const v8::FunctionCallbackInfo<v8::Value> & args) {
+  /* ArrayBuffer buffer = args[0]->ArrayBuffer(); */
+  v8::Isolate *isolate = args.GetIsolate();
+
+  v8::Handle<v8::Value> v = args[0];
+  if (v->IsFloat32Array()) {
+    double value = 0.0;
+    v8::Handle<v8::Float32Array> array = v8::Handle<v8::Float32Array>::Cast(v);
+    for (size_t i = 0; i < array->Length(); i++) {
+      value += array->Get(i)->NumberValue();
+    }
+
+    args.GetReturnValue().Set(v8::Number::New(isolate, value));
+  } else {
+    args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "Not a valid type"));
+  }
+}
+
+void Init(v8::Local<v8::Object> exports) {
   NODE_SET_METHOD(exports, "version", Version);
+  NODE_SET_METHOD(exports, "array_test", ArrayTest);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
 
 }  // namespace add_func
-
-
 
--- tf-test/tf_test.js
+++ tf-test/tf_test.js
@@ -1,2 +1,6 @@
 const addon = require('./build/Release/tf_test');
+
 console.log(addon.version());
+
+let array = new Float32Array([1.1, 2.2, 3.3]);
+console.log(addon.array_test(array));
